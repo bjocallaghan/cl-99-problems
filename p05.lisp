53c5aa2d6c0b4fa07863a61db36f277e32f466f9
Brian O'Callaghan <ocallaghan.brian.j@gmail.com>
2015-03-09 14:53:41 -0500

    changed name: palindrome -> palindromep

--- p05.lisp
+++ p05.lisp
@@ -1,12 +1,12 @@
 (use-package :rt)
 
 (deftest example-1
-    (palindrome '(x a m a x)) t)
+    (paldindromep '(x a m a x)) t)
 
 (deftest example-2
-    (palindrome '(1 2 3 4 5)) nil)
+    (paldindromep '(1 2 3 4 5)) nil)
 
-(defun palindrome (list)
+(defun paldindromep (list)
   (equal (reverse list) list))
 
 (do-tests)
