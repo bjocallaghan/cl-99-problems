4906e3f70876407a667e12b46857e4c61d51ae8e
Brian O'Callaghan <ocallaghan.brian.j@gmail.com>
2015-03-09 20:36:37 -0500

    p15

---
+++ p15.lisp
@@ -0,0 +1,15 @@
+(use-package :rt)
+
+(deftest example
+    (repli '(a b c) 3)
+  (A A A B B B C C C))
+
+(defun repli (list times)
+  (labels ((replicate-item (x)
+             (let (x-list)
+               (dotimes (i times x-list) (push x x-list))))
+           (replicate (list)
+             (when list (nconc (replicate-item (car list)) (replicate (cdr list))))))
+    (replicate list)))
+
+(do-tests)
