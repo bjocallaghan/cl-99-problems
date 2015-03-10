d7bcc9cd9e97582a8463111c3395b2d39fc4d876
Brian O'Callaghan <ocallaghan.brian.j@gmail.com>
2015-03-09 19:48:39 -0500

    p13

--- p13.lisp
+++ p13.lisp
@@ -4,17 +4,21 @@
     (encode-direct '(a a a a b c c a a d e e e e))
   ((4 A) B (2 C) (2 A) D (4 E)))
 
+
 (defun encode-direct (list)
   (labels ((last-sym (acc)
-             if (atom (car acc)) blah blah
-             ;; (cadar acc)
-             
-             )
+             (if (atom (car acc))
+                 (car acc)
+                 (cadar acc)))
+           (incf-acc (acc)
+             (if (atom (car acc))
+                 (cons (list 2 (car acc)) (cdr acc))
+                 (progn (incf (caar acc)) acc)))
            (pack* (list acc)
              (if list
                  (pack* (cdr list) (if (equal (car list) (last-sym acc))
-                                       (progn (incf (caar acc)) acc)
-                                       (cons (list (car list)) acc)))
+                                       (incf-acc acc)
+                                       (cons (car list) acc)))
                  (nreverse acc))))
     (pack* list nil)))
 
