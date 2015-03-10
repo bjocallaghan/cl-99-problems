9d4e9ade7c23bfed4c61458c85b1a65f74b8e85d
Brian O'Callaghan <ocallaghan.brian.j@gmail.com>
2015-03-09 20:11:13 -0500

    added alternate approach

--- p13.lisp
+++ p13.lisp
@@ -5,6 +5,9 @@
   ((4 A) B (2 C) (2 A) D (4 E)))
 
 
+;;; first attempt: i was still in the groove of the previous problems
+;;; i kind of like it, but possibly overcomplicated
+
 (defun encode-direct (list)
   (labels ((last-sym (acc)
              (if (atom (car acc))
@@ -14,12 +17,30 @@
              (if (atom (car acc))
                  (cons (list 2 (car acc)) (cdr acc))
                  (progn (incf (caar acc)) acc)))
-           (pack* (list acc)
+           (encode (list acc)
              (if list
-                 (pack* (cdr list) (if (equal (car list) (last-sym acc))
+                 (encode (cdr list) (if (equal (car list) (last-sym acc))
                                        (incf-acc acc)
                                        (cons (car list) acc)))
                  (nreverse acc))))
-    (pack* list nil)))
+    (encode list nil)))
+
+;; simpler(?) approach; use multiple registers
+
+(defun encode-direct-alternate (list)
+  (let ((start (gensym)))
+    (labels ((encode-subitem (symbol score)
+               (if (= 1 score)
+                   symbol
+                   (list score symbol)))
+             (encode (list current score acc)
+               (if list
+                   (if (equal (car list) current)
+                       (encode (cdr list) current (1+ score) acc)
+                       (encode (cdr list) (car list) 1
+                               (unless (equal current start)
+                                 (cons (encode-subitem current score) acc))))
+                   (cons (encode-subitem current score) acc))))
+      (nreverse (encode list start 0 nil)))))
 
 (do-tests)
