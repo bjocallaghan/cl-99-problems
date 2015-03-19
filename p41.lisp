272fe214673759a6876e7f55224da60e09660bcb
Brian O'Callaghan <ocallaghan.brian.j@gmail.com>
2015-03-18 20:37:25 -0500

    added "search" for larger addends in composition

--- p41.lisp
+++ p41.lisp
@@ -6,10 +6,17 @@
                (if (factor:primep complement)
                    (list (car prime-list) complement)
                    (seek-solution (cdr prime-list))))))
-    (seek-solution (loop for i from 3 to n when (factor:primep i) collect i))))
+    (seek-solution (loop for i from 2 to n when (factor:primep i) collect i))))
 
 (defun goldbach-list (n m)
   (loop for i from n to m when (evenp i)
      do (let ((composition (goldbach i)))
           (format t "~&~d = ~d + ~d~%"
                   i (first composition) (second composition)))))
+
+(defun goldbach-list-large-seeker (n m limit)
+  (loop for i from n to m when (and (> i 2) (evenp i))
+     do (let ((composition (goldbach i)))
+          (when (> (first composition) limit)
+            (format t "~&~d = ~d + ~d~%"
+                    i (first composition) (second composition))))))
